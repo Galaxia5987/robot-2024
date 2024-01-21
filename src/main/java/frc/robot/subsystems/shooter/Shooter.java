8559c3eda0c0fc151d5dfc973cad64d9d6773b58
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-21 19:20:03 +0200

    Create Shooter.java

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Shooter extends SubsystemBase {
+    private static Shooter INSTANCE = null;
+    private final ShooterInputsAutoLogged inputs = ShooterIO.inputs;
+    private final ShooterIO io;
+
+    /**
+     * Constructor for Shooter subsystem.
+     *
+     * @param io IO of the subsystem.
+     */
+    private Shooter(ShooterIO io) {
+        this.io = io;
+    }
+
+    /**
+     * Gets the single instance of Shooter.
+     *
+     * @return The single instance of Shooter.
+     */
+    public static Shooter getInstance() {
+        return INSTANCE;
+    }
+
+    public static void initialize(ShooterIO io) {
+        INSTANCE = new Shooter(io);
+    }
+
+    /**
+     * Sets the velocity of the shooter.
+     *
+     * @param velocity The velocity of the Shooter to set.
+     */
+    public void setVelocity(MutableMeasure<Velocity<Angle>> velocity) {
+        inputs.velocitySetpoint = velocity;
+    }
+
+    public MutableMeasure<Velocity<Angle>> getVelocity() {
+        return inputs.velocity;
+    }
+
+    public boolean atSetpoint() {
+        return io.atSetpoint();
+    }
+
+    /** Updates the state of the shooter. */
+    @Override
+    public void periodic() {
+        io.setVelocity(inputs.velocitySetpoint);
+
+        io.updateInputs();
+        Logger.processInputs("Shooter", inputs);
+    }
+}
