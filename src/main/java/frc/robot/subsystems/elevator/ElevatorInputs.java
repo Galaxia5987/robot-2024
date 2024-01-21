d55db0dc92ba1f0127d5a86a8b7ba058e9af621c
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-21 21:21:27 +0200

    Change to MutableMeasure<Velocity<Velocity<Distance>>>

--- src/main/java/frc/robot/subsystems/elevator/ElevatorInputs.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorInputs.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.elevator;
 
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog
@@ -9,6 +10,6 @@ public class ElevatorInputs {
     public double power;
     public MutableMeasure<Distance> height;
     public MutableMeasure<Distance> heightSetpoint;
-    public double acceleration;
+    public MutableMeasure<Velocity<Velocity<Distance>>> acceleration;
     public ElevatorIO.ControlMode controlMode;
 }
