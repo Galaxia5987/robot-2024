a0891bfe7d81e91ab155d6751a8629964f894ce6
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-21 20:28:44 +0200

    Add Elevator constants class

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.elevator;
+
+public class ElevatorConstants {
+    public static final double MECHANISM_WIDTH = 0;
+    public static final double MECHANISM_HEIGHT = 0;
+    public static final double ROOT_X = 0;
+    public static final double ROOT_Y = 0;
+}
