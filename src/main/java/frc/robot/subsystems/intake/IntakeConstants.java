b38babce7c2115f5fefb513ee61988edc8bcbf20
Alon Schwierz <@1>
2024-01-21 22:17:28 +0200

    spotlessAplly

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,20 +1,21 @@
 package frc.robot.subsystems.intake;
 
+import static edu.wpi.first.units.Units.Radians;
+
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.MutableMeasure;
-import edu.wpi.first.units.Units;
-
-import static edu.wpi.first.units.Units.Radians;
 
 public class IntakeConstants {
     public static final double ANGLE_RATIO = 0;
     public static final MutableMeasure<Angle> DOWN_ANGLE = MutableMeasure.zero(Radians);
     public static final MutableMeasure<Angle> UP_ANGLE = MutableMeasure.zero(Radians);
+
     public enum IntakePose {
         UP(IntakeConstants.UP_ANGLE),
         DOWN(IntakeConstants.DOWN_ANGLE);
         MutableMeasure<Angle> intakePose;
-        private IntakePose(MutableMeasure<Angle> intakePose){
+
+        private IntakePose(MutableMeasure<Angle> intakePose) {
             this.intakePose = intakePose;
         }
     }
