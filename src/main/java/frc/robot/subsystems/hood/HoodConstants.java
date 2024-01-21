c8d028fb0fb2d7b47321f1f2894287767c8f06c9
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-21 20:22:07 +0200

    Add HoodConstants

---
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.hood;
+
+import edu.wpi.first.math.Pair;
+
+public class HoodConstants {
+    public static Pair<Integer, Integer> mechanism2dPose = new Pair<>(1, 1);
+    public static double HoodLength = 0.4; //m
+}
