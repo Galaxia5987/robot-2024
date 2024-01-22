263c505cbffebc4fd331bcd5570e1bf8f2d7c98c
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-22 14:26:55 +0200

    Delete constants

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -3,6 +3,4 @@ package frc.robot.subsystems.elevator;
 public class ElevatorConstants {
     public static final double MECHANISM_WIDTH = 0;
     public static final double MECHANISM_HEIGHT = 0;
-    public static final double ROOT_X = 0;
-    public static final double ROOT_Y = 0;
 }
