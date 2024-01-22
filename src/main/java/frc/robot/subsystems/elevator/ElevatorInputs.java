2921fdc3f5ddf12009a0bf12e0b8eab343409f09
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-22 13:53:29 +0200

    Call process inputs

--- src/main/java/frc/robot/subsystems/elevator/ElevatorInputs.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorInputs.java
@@ -4,12 +4,24 @@ import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Velocity;
 import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 @AutoLog
-public class ElevatorInputs {
+public class ElevatorInputs implements LoggableInputs {
     public double power;
     public MutableMeasure<Distance> height;
     public MutableMeasure<Distance> heightSetpoint;
     public MutableMeasure<Velocity<Velocity<Distance>>> acceleration;
     public ElevatorIO.ControlMode controlMode;
+
+    @Override
+    public void toLog(LogTable table) {
+
+    }
+
+    @Override
+    public void fromLog(LogTable table) {
+
+    }
 }
