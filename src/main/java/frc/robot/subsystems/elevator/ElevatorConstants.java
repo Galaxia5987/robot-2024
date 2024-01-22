261446489e8ac4dc473ab2f9149feaec5eb5f28a
vichik123 <eitan.vichik@gmail.com>
2024-01-22 22:20:36 +0200

    Delete unnecessary elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.elevator;
-
-public class ElevatorConstants {
-    public static final double MECHANISM_WIDTH = 0;
-    public static final double MECHANISM_HEIGHT = 0;
-}
