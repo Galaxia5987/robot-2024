af147931adabf113d42510997dd3a4c1bde56a7e
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-22 19:52:20 +0200

    Implement methods is sim&real

--- src/main/java/frc/robot/subsystems/gripper/GriperIOReal.java
+++ src/main/java/frc/robot/subsystems/gripper/GriperIOReal.java
@@ -1,3 +1,26 @@
 package frc.robot.subsystems.gripper;
 
-public class GriperIOReal extends GripperInputs {}
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.MutableMeasure;
+
+public class GriperIOReal implements GripperIO {
+    @Override
+    public void setSpeedMotorPower(double power) {
+
+    }
+
+    @Override
+    public void setAngleMotorPower(double power) {
+
+    }
+
+    @Override
+    public void setAngle(MutableMeasure<Angle> angle) {
+
+    }
+
+    @Override
+    public void updateInputs(GripperInputs inputs) {
+
+    }
+}
--- src/main/java/frc/robot/subsystems/gripper/GriperIOSim.java
+++ src/main/java/frc/robot/subsystems/gripper/GriperIOSim.java
@@ -1,3 +1,26 @@
 package frc.robot.subsystems.gripper;
 
-public class GriperIOSim {}
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.MutableMeasure;
+
+public class GriperIOSim implements GripperIO{
+    @Override
+    public void setSpeedMotorPower(double power) {
+
+    }
+
+    @Override
+    public void setAngleMotorPower(double power) {
+
+    }
+
+    @Override
+    public void setAngle(MutableMeasure<Angle> angle) {
+
+    }
+
+    @Override
+    public void updateInputs(GripperInputs inputs) {
+
+    }
+}
