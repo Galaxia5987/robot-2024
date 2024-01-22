8cb7b68ea5d3961a8603a2e26c5a8b1f04b74b5c
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-22 20:52:04 +0200

    Add control type

--- src/main/java/frc/robot/subsystems/gripper/Gripper.java
+++ src/main/java/frc/robot/subsystems/gripper/Gripper.java
@@ -7,22 +7,20 @@ import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class Gripper extends SubsystemBase {
     private static Gripper INSTANCE;
-    private final GripperInputs inputs = new GripperInputs();
     private final GripperIO io;
+    public GripperIO.ControlMode controlMode;
 
-    @AutoLogOutput
-    private final Mechanism2d mechanism2d = new Mechanism2d(0, 0);
+    @AutoLogOutput private final Mechanism2d mechanism2d = new Mechanism2d(0, 0);
 
     private final MechanismRoot2d root = mechanism2d.getRoot("Gripper", 0, 0);
     private final MechanismLigament2d gripper =
             root.append(new MechanismLigament2d("Gripper", 0, 0));
-    private final GripperInputsAutoLogged input = new GripperInputsAutoLogged();
+    private final GripperInputsAutoLogged inputs = new GripperInputsAutoLogged();
 
     public Gripper(GripperIO io) {
         this.io = io;
@@ -30,15 +28,17 @@ public class Gripper extends SubsystemBase {
 
     public void setSpeedMotorPower(double power) {
         io.setSpeedMotorPower(power);
-        
+        controlMode = GripperIO.ControlMode.PERCENT_OUTPUT;
     }
 
     public void setAngleMotorPower(double power) {
         io.setAngleMotorPower(power);
+        controlMode = GripperIO.ControlMode.PERCENT_OUTPUT;
     }
 
     public void setAngle(MutableMeasure<Angle> angle) {
         io.setAngle(angle);
+        controlMode = GripperIO.ControlMode.POSITION;
     }
 
     public MutableMeasure<Angle> getCurrentAngle() {
@@ -53,18 +53,18 @@ public class Gripper extends SubsystemBase {
         return inputs.hasNote;
     }
 
-    public MutableMeasure<Voltage> spinMotorVoltage() {
+    public MutableMeasure<Voltage> getSpinMotorVoltage() {
         return inputs.spinMotorVoltage;
     }
 
-    public MutableMeasure<Voltage> angleMotorVoltage() {
+    public MutableMeasure<Voltage> getAngleMotorVoltage() {
         return inputs.angleMotorVoltage;
     }
 
     @Override
     public void periodic() {
         io.updateInputs(inputs);
-        Logger.processInputs(this.getClass().getSimpleName(), input);
+        Logger.processInputs(this.getClass().getSimpleName(), inputs);
 
         Logger.recordOutput("current angle", getCurrentAngle());
     }
