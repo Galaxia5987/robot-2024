1ba955c10495255233d51292faea3c4d6242316a
Emma <emmalivne.03@gmail.com>
2024-01-23 20:30:04 +0200

    Add constants

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,6 +1,15 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorConstants {
+public class ElevatorConstants { // TODO: check real values
     public static final double MECHANISM_WIDTH = 0;
     public static final double MECHANISM_HEIGHT = 0;
+    public static final double GEAR_RATIO = 1 / 25;
+    public static final double DRUM_RADIUS = 0;
+    public static final double MIN_HEIGHT = 0;
+    public static final double MAX_HEIGHT = 0;
+    public static final double STARTING_HEIGHT = 0;
+    public static final double GRAVITY = 0;
+    public static final double KP = 0;
+    public static final double KI = 0;
+    public static final double KD = 0;
 }
