aff8ea89372293051ab1f2bf3dbfea796bf031af
GaiaZano05 <gaya.zano11@gmail.com>
2024-01-24 17:52:23 +0200

    Make constants mutable

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -4,14 +4,17 @@ import edu.wpi.first.units.*;
 import lib.webconstants.LoggedTunableNumber;
 
 public class ElevatorConstants { // TODO: check real values
-    public static final double MECHANISM_WIDTH = 0; //[m]
-    public static final double MECHANISM_HEIGHT = 0; //[m]
+    public static final double MECHANISM_WIDTH = 0; // [m]
+    public static final double MECHANISM_HEIGHT = 0; // [m]
     public static final double GEAR_RATIO = 1 / 12;
-    public static final double DRUM_RADIUS = 0.02; //[m]
+    public static final double DRUM_RADIUS = 0.02; // [m]
 
-    public static final MutableMeasure<Distance> STARTING_HEIGHT = Units.Meters.of(0).mutableCopy(); //[m]
-    public static final MutableMeasure<Distance> MIN_HEIGHT = Units.Meters.of(0).mutableCopy(); //[m]
-    public static final MutableMeasure<Distance> MAX_HEIGHT = Units.Meters.of(0).mutableCopy(); //[m]
+    public static final MutableMeasure<Distance> STARTING_HEIGHT =
+            Units.Meters.of(0).mutableCopy(); // [m]
+    public static final MutableMeasure<Distance> MIN_HEIGHT =
+            Units.Meters.of(0).mutableCopy(); // [m]
+    public static final MutableMeasure<Distance> MAX_HEIGHT =
+            Units.Meters.of(0).mutableCopy(); // [m]
 
     public static final LoggedTunableNumber KP = new LoggedTunableNumber("kp", 0);
     public static final LoggedTunableNumber KI = new LoggedTunableNumber("ki", 0);
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.elevator;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
 import edu.wpi.first.math.controller.PIDController;
@@ -7,8 +9,6 @@ import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.wpilibj.Timer;
 import lib.motors.TalonFXSim;
-
-import static edu.wpi.first.units.Units.Meters;
 
 public class ElevatorIOSim implements ElevatorIO {
     private final TalonFXSim motor;
@@ -23,7 +23,10 @@ public class ElevatorIOSim implements ElevatorIO {
 
         motor.setController(
                 new PIDController(
-                        ElevatorConstants.KP.get(), ElevatorConstants.KI.get(), ElevatorConstants.KD.get(), 0.02));
+                        ElevatorConstants.KP.get(),
+                        ElevatorConstants.KI.get(),
+                        ElevatorConstants.KD.get(),
+                        0.02));
     }
 
     @Override
@@ -43,8 +46,8 @@ public class ElevatorIOSim implements ElevatorIO {
         motor.update(Timer.getFPGATimestamp());
         inputs.currentHeight =
                 Meters.of(
-                        lib.units.Units.rpsToMetersPerSecond(
-                                motor.getPosition(), ElevatorConstants.DRUM_RADIUS))
+                                lib.units.Units.rpsToMetersPerSecond(
+                                        motor.getPosition(), ElevatorConstants.DRUM_RADIUS))
                         .mutableCopy();
     }
 }
