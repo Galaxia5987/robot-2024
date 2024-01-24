d8fb3a02238c3de747945b43b22cd8baffd009fb
eitan <eitan.vichik@gmail.com>
2024-01-24 17:33:54 +0200

    Fix compilation bug

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -5,6 +5,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class HoodConstants {
     public static Translation2d mechanism2dPose = new Translation2d(1, 1);
     public static double HoodLength = 0.4; // m
-    public static double velocityTolerance = 0.5;
+    public static double positionTolerance = 0.05;
     public static double resetPower = -0.1; // RPS
 }
