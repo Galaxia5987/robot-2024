58557de7167bb3df7708728859f918dfbff58f03
vichik123 <eitan.vichik@gmail.com>
2024-01-26 11:30:12 +0200

    Initialize swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,25 +2,47 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.example.*;
+import frc.robot.swerve.*;
 
 public class RobotContainer {
 
     private static RobotContainer INSTANCE = null;
 
+    private final SwerveDrive swerveDrive;
+
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     private RobotContainer() {
         ExampleSubsystemIO exampleSubsystemIO;
+        ModuleIO[] moduleIOs = new ModuleIO[4];
+        GyroIO gyroIO;
         switch (Constants.CURRENT_MODE) {
             case REAL:
                 exampleSubsystemIO = new ExampleSubsystemIOReal();
+                for (int i = 0; i < moduleIOs.length; i++) {
+                    moduleIOs[i] =
+                            new ModuleIOTalonFX(
+                                    Ports.SwerveDrive.DRIVE_IDS[i],
+                                    Ports.SwerveDrive.ANGLE_IDS[i],
+                                    Ports.SwerveDrive.ENCODER_IDS[i],
+                                    SwerveConstants.DRIVE_MOTOR_CONFIGS,
+                                    SwerveConstants.ANGLE_MOTOR_CONFIGS);
+                }
+                gyroIO = new GyroIOReal();
                 break;
             case SIM:
             case REPLAY:
             default:
                 exampleSubsystemIO = new ExampleSubsystemIOSim();
+                for (int i = 0; i < moduleIOs.length; i++) {
+                    moduleIOs[i] = new ModuleIOSim();
+                }
+                gyroIO = new GyroIOSim();
                 break;
         }
         ExampleSubsystem.initialize(exampleSubsystemIO);
+        SwerveDrive.initialize(gyroIO, moduleIOs);
+
+        swerveDrive = SwerveDrive.getInstance();
 
         // Configure the button bindings and default commands
         configureDefaultCommands();
