0d499e8853355b003970a0ca70865d8023c5cbbb
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-27 11:56:07 +0200

    Remove Shooter from RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,40 +1,30 @@
 package frc.robot;
 
-import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import frc.robot.subsystems.example.*;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.shooter.ShooterIO;
-import frc.robot.subsystems.shooter.ShooterIOSim;
 
 public class RobotContainer {
 
     private static RobotContainer INSTANCE = null;
 
-    private final Shooter shooter;
     private final CommandGenericHID keyboard = new CommandGenericHID(0);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     private RobotContainer() {
         ExampleSubsystemIO exampleSubsystemIO;
-        ShooterIO shooterIO;
+
         switch (Constants.CURRENT_MODE) {
             case REAL:
                 exampleSubsystemIO = new ExampleSubsystemIOReal();
-                shooterIO = new ShooterIOSim();
                 break;
             case SIM:
             case REPLAY:
             default:
                 exampleSubsystemIO = new ExampleSubsystemIOSim();
-                shooterIO = new ShooterIOSim();
                 break;
         }
         ExampleSubsystem.initialize(exampleSubsystemIO);
-        Shooter.initialize(shooterIO);
-
-        shooter = Shooter.getInstance();
 
         // Configure the button bindings and default commands
         configureDefaultCommands();
@@ -50,10 +40,7 @@ public class RobotContainer {
 
     private void configureDefaultCommands() {}
 
-    private void configureButtonBindings() {
-        shooter.setDefaultCommand(
-                shooter.setVelocity(() -> Units.RotationsPerSecond.of(100).mutableCopy()));
-    }
+    private void configureButtonBindings() {}
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
