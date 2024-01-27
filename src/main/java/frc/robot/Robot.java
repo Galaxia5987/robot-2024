abf071752c00554c774bc15a416a033fd5d10b95
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-27 11:11:35 +0200

    Revert "Add shooter constants init to Robot"
    This reverts commit c159aa1240c068a1a14e14f1376363c781c3821e.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.example.ExampleSubsystemConstants;
-import frc.robot.subsystems.shooter.ShooterConstants;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -78,7 +77,6 @@ public class Robot extends LoggedRobot {
         SignalLogger.enableAutoLogging(true);
 
         ExampleSubsystemConstants.initConstants();
-        ShooterConstants.initialize(Constants.CURRENT_MODE);
         robotContainer = RobotContainer.getInstance();
         compressor.enableDigital();
     }
