573e8ee0daf9454a426c481d0bafc8fae7344ac1
eitan <eitan.vichik@gmail.com>
2024-01-28 21:07:58 +0200

    Merge branch 'develop' into feature/shooter-sim

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,19 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
-import frc.robot.subsystems.example.*;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.example.ExampleSubsystem;
+import frc.robot.subsystems.example.ExampleSubsystemIO;
+import frc.robot.subsystems.example.ExampleSubsystemIOReal;
+import frc.robot.subsystems.example.ExampleSubsystemIOSim;
+import frc.robot.swerve.*;
 
 public class RobotContainer {
 
     private static RobotContainer INSTANCE = null;
 
-    private final CommandGenericHID keyboard = new CommandGenericHID(0);
+    private final SwerveDrive swerveDrive;
+    private final CommandXboxController xboxController = new CommandXboxController(0);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     private RobotContainer() {
@@ -25,6 +30,10 @@ public class RobotContainer {
                 break;
         }
         ExampleSubsystem.initialize(exampleSubsystemIO);
+        Constants.initSwerve();
+        Constants.initVision();
+
+        swerveDrive = SwerveDrive.getInstance();
 
         // Configure the button bindings and default commands
         configureDefaultCommands();
@@ -38,7 +47,15 @@ public class RobotContainer {
         return INSTANCE;
     }
 
-    private void configureDefaultCommands() {}
+    private void configureDefaultCommands() {
+        swerveDrive.setDefaultCommand(
+                swerveDrive.driveCommand(
+                        () -> -xboxController.getLeftY(),
+                        () -> -xboxController.getLeftX(),
+                        () -> -xboxController.getRightX(),
+                        0.15,
+                        () -> true));
+    }
 
     private void configureButtonBindings() {}
 
