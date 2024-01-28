a31211900661ba57d1cceea50b0bf3119f1d7faf
vichik123 <eitan.vichik@gmail.com>
2024-01-28 19:44:21 +0200

    Add init

--- src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
@@ -1,6 +1,21 @@
 package frc.robot.subsystems.conveyor;
 
 public class Conveyor {
-    
+
+    private static Conveyor INSTANCE = null;
+
+    private final ConveyorIO io;
     private final ConveyorInputsAutoLogged inputs = ConveyorIO.inputs;
+
+    private Conveyor(ConveyorIO io) {
+        this.io = io;
+    }
+
+    public static Conveyor getInstance() {
+        return INSTANCE;
+    }
+
+    public static void initialize(ConveyorIO io) {
+        INSTANCE = new Conveyor(io);
+    }
 }
