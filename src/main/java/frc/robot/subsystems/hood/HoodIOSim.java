2c28e133cae797c390664b8cf1b14375b7ad2208
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-28 17:20:47 +0200

    Create HoodIOSim

---
+++ src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems.hood;
+
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.Timer;
+import lib.motors.TalonFXSim;
+
+public class HoodIOSim implements HoodIO {
+    private final TalonFXSim motor;
+
+    private final PositionVoltage control = new PositionVoltage(0);
+    private final SimpleMotorFeedforward motorFeedforward;
+
+    public HoodIOSim() {
+        motor = new TalonFXSim(1, HoodConstants.GEAR_RATIO, HoodConstants.MOMENT_OF_INERTIA, 1);
+
+        motor.setController(
+                new PIDController(
+                        HoodConstants.kP.get(), HoodConstants.kI.get(), HoodConstants.kD.get()));
+
+        motorFeedforward =
+                new SimpleMotorFeedforward(
+                        HoodConstants.kS.get(), HoodConstants.kV.get(), HoodConstants.kA.get());
+    }
+
+    @Override
+    public void updateInternalEncoder() {}
+
+    @Override
+    public void setAngle(MutableMeasure<Angle> angle) {
+        inputs.controlMode = Mode.ANGLE;
+        inputs.angleSetpoint = angle.mut_replace(angle);
+        motor.setControl(
+                control.withPosition(angle.in(Units.Radians))
+                        .withFeedForward(
+                                motorFeedforward.calculate(
+                                        angle.in(Units.RotationsPerSecond.getUnit()))));
+    }
+
+    @Override
+    public void setPower(double power) {
+        inputs.controlMode = Mode.POWER;
+        inputs.powerSetpoint = power;
+        motor.setControl(new VelocityVoltage(power));
+    }
+
+    @Override
+    public void updateInputs() {
+        motor.update(Timer.getFPGATimestamp());
+
+        inputs.angle.mut_replace(motor.getPosition(), Units.Radians);
+        inputs.voltage.mut_replace(motor.getAppliedVoltage(), Units.Volts);
+    }
+}
