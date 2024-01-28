ff20c870316b2ae353bf430d679e4296fe4ec647
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-28 20:39:51 +0200

    change hood length constant to measure

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.hood;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Dimensionless;
+import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
 import frc.robot.Constants;
@@ -9,7 +10,7 @@ import lib.webconstants.LoggedTunableNumber;
 
 public class HoodConstants {
     public static Translation2d MECHANISM_2D_POSE = new Translation2d(1, 1);
-    public static double HOOD_LENGTH = 0.4; // m
+    public static Measure<Distance> HOOD_LENGTH = Units.Meters.of(0.4);
     public static Measure<Dimensionless> POSITION_TOLERANCE = Units.Percent.of(5);
     public static final double GEAR_RATIO = 1.0;
     public static final double MOMENT_OF_INERTIA = 0.08; // TODO: check what is the correct value
