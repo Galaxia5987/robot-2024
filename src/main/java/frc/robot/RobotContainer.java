5703c7fe03c04fb3a3f942d77b70f0869886b3a4
Alon Schwierz <@1>
2024-01-29 19:35:31 +0200

    Merge branch 'feature/intake-sim' of https://github.com/Galaxia5987/robot-2024 into feature/intake-sim

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.units.Units;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.example.ExampleSubsystem;
@@ -9,9 +7,6 @@ import frc.robot.subsystems.example.ExampleSubsystemIO;
 import frc.robot.subsystems.example.ExampleSubsystemIOReal;
 import frc.robot.subsystems.example.ExampleSubsystemIOSim;
 import frc.robot.swerve.*;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.intake.IntakeIO;
-import frc.robot.subsystems.intake.IntakeIOSim;
 
 public class RobotContainer {
 
@@ -24,22 +19,17 @@ public class RobotContainer {
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     private RobotContainer() {
         ExampleSubsystemIO exampleSubsystemIO;
-        IntakeIO intakeIO;
         switch (Constants.CURRENT_MODE) {
             case REAL:
                 exampleSubsystemIO = new ExampleSubsystemIOReal();
-                intakeIO = new IntakeIOSim();
                 break;
             case SIM:
             case REPLAY:
             default:
                 exampleSubsystemIO = new ExampleSubsystemIOSim();
-                intakeIO = new IntakeIOSim();
                 break;
         }
         ExampleSubsystem.initialize(exampleSubsystemIO);
-        Intake.initialize(intakeIO);
-        intake = Intake.getInstance();
         Constants.initSwerve();
         Constants.initVision();
 
