31e3db824d1a9ef2e664817cb5d780a09aa9ac59
Alon Schwierz <@1>
2024-01-30 16:41:17 +0200

    fix null value for intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,41 +1,50 @@
 package frc.robot;
 
 import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.example.ExampleSubsystem;
 import frc.robot.subsystems.example.ExampleSubsystemIO;
 import frc.robot.subsystems.example.ExampleSubsystemIOReal;
 import frc.robot.subsystems.example.ExampleSubsystemIOSim;
 import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.IntakeConstants;
+import frc.robot.subsystems.intake.IntakeIO;
+import frc.robot.subsystems.intake.IntakeIOSim;
 import frc.robot.swerve.*;
 
 public class RobotContainer {
 
     private static RobotContainer INSTANCE = null;
     private Intake intake;
-
     private final SwerveDrive swerveDrive;
     private final CommandXboxController xboxController = new CommandXboxController(0);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     private RobotContainer() {
         ExampleSubsystemIO exampleSubsystemIO;
+        IntakeIO intakeIO;
         switch (Constants.CURRENT_MODE) {
             case REAL:
                 exampleSubsystemIO = new ExampleSubsystemIOReal();
+                intakeIO = new IntakeIOSim();
                 break;
             case SIM:
             case REPLAY:
             default:
                 exampleSubsystemIO = new ExampleSubsystemIOSim();
+                intakeIO = new IntakeIOSim();
                 break;
         }
         ExampleSubsystem.initialize(exampleSubsystemIO);
+        Intake.initialize(intakeIO);
         Constants.initSwerve();
         Constants.initVision();
 
         swerveDrive = SwerveDrive.getInstance();
+        intake = Intake.getInstance();
 
         // Configure the button bindings and default commands
         configureDefaultCommands();
