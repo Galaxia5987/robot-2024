fe871abe14bd8266269cb3bf6d436627c0e2cbf0
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-30 15:58:48 +0200

    Add class rollerInputs

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -6,7 +6,8 @@ import edu.wpi.first.units.*;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ShooterIO {
-    ShooterInputsAutoLogged inputs = new ShooterInputsAutoLogged();
+    TopRollerInputsAutoLogged topRollerInputs = new TopRollerInputsAutoLogged();
+    BottomRollerInputsAutoLogged bottomRollerInputs = new BottomRollerInputsAutoLogged();
 
     void setTopVelocity(MutableMeasure<Velocity<Angle>> velocity);
 
@@ -17,17 +18,12 @@ public interface ShooterIO {
     /** Update the inputs of the Shooter */
     void updateInputs();
 
-    @AutoLog
-    class ShooterInputs {
-        public MutableMeasure<Velocity<Angle>> topVelocity =
+    class RollerInputs {
+        public MutableMeasure<Velocity<Angle>> velocity = MutableMeasure.zero(RotationsPerSecond);
+        public MutableMeasure<Velocity<Angle>> velocitySetpoint =
                 MutableMeasure.zero(RotationsPerSecond);
-        public MutableMeasure<Velocity<Angle>> bottomVelocity =
-                MutableMeasure.zero(RotationsPerSecond);
-        public MutableMeasure<Velocity<Angle>> topVelocitySetpoint =
-                MutableMeasure.zero(RotationsPerSecond);
-        public MutableMeasure<Velocity<Angle>> bottomVelocitySetpoint =
-                MutableMeasure.zero(RotationsPerSecond);
-        public MutableMeasure<Voltage> topVoltage = MutableMeasure.zero(Volts);
-        public MutableMeasure<Voltage> bottomVoltage = MutableMeasure.zero(Volts);
+        public MutableMeasure<Voltage> voltage = MutableMeasure.zero(Volts);
+
+        public RollerInputs() {}
     }
 }
