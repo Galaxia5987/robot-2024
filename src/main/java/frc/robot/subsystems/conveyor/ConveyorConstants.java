aea59b41238ba4f2481c1f1642bfb26eb27905c0
Alon Schwierz <@1>
2024-01-30 17:43:31 +0200

    .

--- src/main/java/frc/robot/subsystems/conveyor/ConveyorConstants.java
+++ src/main/java/frc/robot/subsystems/conveyor/ConveyorConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.conveyor;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.*;
+import edu.wpi.first.wpilibj.Timer;
+import lib.webconstants.LoggedTunableNumber;
 
 public class ConveyorConstants {
 
@@ -9,4 +12,9 @@ public class ConveyorConstants {
     public static final MutableMeasure<Dimensionless> SETPOINT_TOLERANCE =
             Units.Value.of(0.05).mutableCopy();
     public static final double GEAR_RATIO = 0;
+    public static LoggedTunableNumber KP = new LoggedTunableNumber("Conveyor/kP", 0);
+    public static LoggedTunableNumber KI = new LoggedTunableNumber("Conveyor/kI", 0);
+    public static LoggedTunableNumber KD = new LoggedTunableNumber("Conveyor/kD", 0);
+
+    public static PIDController CONTROLLER = new PIDController(KP.get(), KI.get(), KD.get(), Timer.getFPGATimestamp());
 }
--- src/main/java/frc/robot/subsystems/conveyor/ConveyorIOSim.java
+++ src/main/java/frc/robot/subsystems/conveyor/ConveyorIOSim.java
@@ -1,27 +1,33 @@
 package frc.robot.subsystems.conveyor;
 
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.Velocity;
-import frc.robot.Robot;
+import edu.wpi.first.wpilibj.Timer;
 import lib.motors.SparkMaxSim;
 
-import java.util.Timer;
+public class ConveyorIOSim implements ConveyorIO {
+    private final SparkMaxSim conveyor;
 
-public class ConveyorIOSim implements ConveyorIO{
-private final SparkMaxSim conveyor;
-
-public ConveyorIOSim() {
-    conveyor = new SparkMaxSim(1, ConveyorConstants.GEAR_RATIO, 0.5, 0);
-}
+    public ConveyorIOSim() {
+        conveyor = new SparkMaxSim(1, ConveyorConstants.GEAR_RATIO, 0.5, ConveyorConstants.GEAR_RATIO);
+        conveyor.setController(ConveyorConstants.CONTROLLER);
+        conveyor.setProfiledController();
+    }
 
     @Override
     public void setVelocity(MutableMeasure<Velocity<Angle>> velocity) {
-
+        conveyor.setReference(velocity.in(Units.RotationsPerSecond), CANSparkMax.ControlType.kVelocity);
     }
 
     @Override
     public void updateInputs() {
-conveyor.update();
+        conveyor.update(Timer.getFPGATimestamp());
+        inputs.velocity.mut_replace(Units.RotationsPerSecond.of(conveyor.getVelocity()));
+        inputs.appliedVoltage.mut_replace(Units.Volts.of(conveyor.getBusVoltage()));
+        inputs.appliedCurrent.mut_replace(Units.Amps.of(conveyor.getAppliedOutput()));
     }
 }
