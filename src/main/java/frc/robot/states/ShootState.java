205a37ec7f2b58730a7e1e66e18068eaa46cdb79
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-02 11:20:07 +0200

    Add ShootState

---
+++ src/main/java/frc/robot/states/ShootState.java
@@ -0,0 +1,36 @@
+package frc.robot.states;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.swerve.SwerveDrive;
+import java.util.Arrays;
+import lib.Utils;
+
+class ShootState implements ScoreState {
+
+    @Override
+    public Command driveToClosestOptimalPoint() {
+        return Commands.defer(
+                () -> {
+                    Pose2d optimalPose =
+                            Utils.calcOptimalPose(
+                                    Arrays.asList(Constants.OPTIMAL_POINTS_SHOOT),
+                                    SwerveDrive.getInstance().getBotPose());
+                    return AutoBuilder.pathfindToPose(optimalPose, Constants.AUTO_CONSTRAINTS);
+                },
+                DTOP_REQUIREMENTS);
+    }
+
+    @Override
+    public Command stateInitialize() {
+        return null;
+    }
+
+    @Override
+    public Command score() {
+        return null;
+    }
+}
