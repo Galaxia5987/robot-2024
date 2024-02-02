76b990a90a1549ccc7cf2d45c61e578fea1ed188
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-02 11:19:57 +0200

    Add AmpState

---
+++ src/main/java/frc/robot/states/AmpState.java
@@ -0,0 +1,42 @@
+package frc.robot.states;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.swerve.SwerveDrive;
+import java.util.Arrays;
+import java.util.HashSet;
+import lib.Utils;
+
+class AmpState implements ScoreState {
+
+    @Override
+    public Command driveToClosestOptimalPoint() {
+        return Commands.defer(
+                () -> {
+                    Pose2d optimalPose =
+                            Utils.calcOptimalPose( // TODO: make the robot not turn all the way
+                                    // if it is not needed
+                                    Arrays.asList(Constants.OPTIMAL_POINTS_TRAP),
+                                    SwerveDrive.getInstance().getBotPose());
+                    return AutoBuilder.pathfindToPose(optimalPose, Constants.AUTO_CONSTRAINTS);
+                },
+                new HashSet<>() {
+                    {
+                        add(SwerveDrive.getInstance());
+                    }
+                });
+    }
+
+    @Override
+    public Command stateInitialize() {
+        return null;
+    }
+
+    @Override
+    public Command score() {
+        return null;
+    }
+}
