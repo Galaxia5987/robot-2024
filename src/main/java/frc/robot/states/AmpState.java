86771861bf88aeddb5ef035012909fb601b1ac56
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-02 14:51:38 +0200

    Add imports

--- src/main/java/frc/robot/states/AmpState.java
+++ src/main/java/frc/robot/states/AmpState.java
@@ -2,12 +2,13 @@ package frc.robot.states;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
 import frc.robot.swerve.SwerveDrive;
-import java.util.Arrays;
-import java.util.HashSet;
 import lib.Utils;
 
 public class AmpState implements ScoreState {
