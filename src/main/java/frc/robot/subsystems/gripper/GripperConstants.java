919e0145ad51559a593e1838fec62392b82a09e6
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-02 13:49:21 +0200

    Update gearing

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -15,7 +15,7 @@ public class GripperConstants {
     public static final Measure<Distance> GRIPPER_POSITION_X = Units.Meters.of(0);
     public static final Measure<Distance> GRIPPER_POSITION_Y = Units.Meters.of(0);
     public static final Measure<Distance> GRIPPER_POSITION_z = Units.Meters.of(0.6461);
-    public static final double ANGLE_MOTOR_GEAR_RATIO = 1;
+    public static final double ANGLE_MOTOR_GEAR_RATIO = 58.5;
 
     public static final LoggedTunableNumber KP = new LoggedTunableNumber("Gripper/kP");
     public static final LoggedTunableNumber KI = new LoggedTunableNumber("Gripper/kI");
