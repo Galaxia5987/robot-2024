aad898fb6db80574912419e1037d0e04d5a9627c
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-03 18:20:55 +0200

    Spotless

--- src/main/java/frc/robot/states/ScoreState.java
+++ src/main/java/frc/robot/states/ScoreState.java
@@ -3,7 +3,7 @@ package frc.robot.states;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.swerve.SwerveDrive;
-import java.util.HashSet;
+import java.util.Set;
 
 public interface ScoreState {
     Set<Subsystem> DTOP_REQUIREMENTS = Set.of(SwerveDrive.getInstance());
