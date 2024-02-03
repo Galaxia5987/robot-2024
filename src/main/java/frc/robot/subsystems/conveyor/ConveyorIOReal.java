5e631cbe45f9ae245776f1223beea248c8f36f5a
Alon Schwierz <@1>
2024-02-03 20:27:31 +0200

    Create class

---
+++ src/main/java/frc/robot/subsystems/conveyor/ConveyorIOReal.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.conveyor;
+
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
+
+public class ConveyorIOReal implements ConveyorIO{
+
+
+    
+
+
+    @Override
+    public void setVelocity(MutableMeasure<Velocity<Angle>> velocity) {
+
+    }
+
+    @Override
+    public void updateInputs() {
+
+    }
+}
