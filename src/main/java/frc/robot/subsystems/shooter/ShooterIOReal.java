1f3dffe72236c503e72f34cd992204a2781d7104
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-03 18:06:49 +0200

    Add topMotor configuration

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
@@ -5,6 +5,36 @@ import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Velocity;
 
 public class ShooterIOReal implements ShooterIO {
+    private final TalonFX topMotor;
+    private final TalonFX bottomMotor;
+    private final TalonFXConfigurator topMotorConfigurator;
+    private final TalonFXConfigurator bottomMotorConfigurator;
+    private final TalonFXConfiguration topMotorConfiguration = new TalonFXConfiguration();
+    private final TalonFXConfiguration bottomMotorConfiguration = new TalonFXConfiguration();
+    private MotionMagicVelocityTorqueCurrentFOC topControl =
+            new MotionMagicVelocityTorqueCurrentFOC(0);
+    private MotionMagicVelocityTorqueCurrentFOC bottomControl =
+            new MotionMagicVelocityTorqueCurrentFOC(0);
+
+    public ShooterIOReal() {
+        topMotor = new TalonFX(3); // TODO: to be changed later
+        bottomMotor = new TalonFX(5); // TODO: to be changed later
+
+        topMotor.setNeutralMode(NeutralModeValue.Coast);
+
+        topMotorConfiguration.Feedback.SensorToMechanismRatio = ShooterConstants.GEAR_RATIO_TOP;
+
+        topMotorConfigurator = topMotor.getConfigurator();
+        topMotorConfiguration.Slot0.kP = ShooterConstants.TOP_kP.get();
+        topMotorConfiguration.Slot0.kI = ShooterConstants.TOP_kI.get();
+        topMotorConfiguration.Slot0.kD = ShooterConstants.TOP_kD.get();
+        topMotorConfiguration.Slot0.kS = ShooterConstants.TOP_kS.get();
+        topMotorConfiguration.Slot0.kV = ShooterConstants.TOP_kV.get();
+        topMotorConfiguration.Slot0.kA = ShooterConstants.TOP_kA.get();
+        topMotorConfiguration.Slot0.kG = ShooterConstants.TOP_kG.get();
+        topMotorConfigurator.apply(topMotorConfiguration.Slot0);
+    }
+
     @Override
     public void setTopVelocity(MutableMeasure<Velocity<Angle>> velocity) {
         topRollerInputs.velocitySetpoint.mut_replace(velocity);
