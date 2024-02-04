1877d9f64cc33f7e29e6fc63e45e77f4f159c39e
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-04 14:50:16 +0200

    Fix from radians to rotations

--- src/main/java/frc/robot/subsystems/gripper/GripperIOSim.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperIOSim.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.gripper;
 
-import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.*;
 import static frc.robot.subsystems.gripper.GripperConstants.*;
 
 import com.ctre.phoenix6.controls.DutyCycleOut;
@@ -8,8 +8,10 @@ import com.ctre.phoenix6.controls.PositionVoltage;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Unit;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.Timer;
+import lib.Utils;
 import lib.motors.TalonFXSim;
 
 public class GripperIOSim implements GripperIO {
@@ -26,7 +28,7 @@ public class GripperIOSim implements GripperIO {
                 new TalonFXSim(
                         1,
                         GripperConstants.ANGLE_MOTOR_GEAR_RATIO,
-                        0.00001,
+                        0.000_01,
                         GripperConstants.ANGLE_MOTOR_GEAR_RATIO);
 
         rollerMotor = new TalonFXSim(1, 1, 0.5, 1);
@@ -46,13 +48,16 @@ public class GripperIOSim implements GripperIO {
 
     @Override
     public void setAngle(MutableMeasure<Angle> angle) {
-        angleMotor.setControl(positionRequest.withPosition(Math.IEEEremainder(angle.in(Radians), 2 * Math.PI)));
+        inputs.angleSetpoint = angle;
+
+        angleMotor.setControl(
+                positionRequest.withPosition(angle.mut_replace(Utils.normalize(angle.in(Radians)), Radians).in(Rotations)));
     }
 
     @Override
     public void updateInputs() {
         rollerMotor.update(Timer.getFPGATimestamp());
         angleMotor.update(Timer.getFPGATimestamp());
-        inputs.currentAngle = Radians.of(angleMotor.getPosition()).mutableCopy();
+        inputs.currentAngle = Rotations.of(angleMotor.getPosition()).mutableCopy();
     }
 }
