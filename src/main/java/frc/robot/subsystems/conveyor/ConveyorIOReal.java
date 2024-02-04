2c739998166620f186d5e8706ce09d5a2fc4e363
Alon Schwierz <@1>
2024-02-04 17:31:21 +0200

    Create Real class

--- src/main/java/frc/robot/subsystems/conveyor/ConveyorIOReal.java
+++ src/main/java/frc/robot/subsystems/conveyor/ConveyorIOReal.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems.conveyor;
 
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.*;
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Velocity;
 
 public class ConveyorIOReal implements ConveyorIO{
 
-
-    
-
+private CANSparkMax roller = new CANSparkMax(1, CANSparkLowLevel.MotorType.kBrushless);
 
     @Override
     public void setVelocity(MutableMeasure<Velocity<Angle>> velocity) {
@@ -17,6 +17,8 @@ public class ConveyorIOReal implements ConveyorIO{
 
     @Override
     public void updateInputs() {
-
+inputs.velocitySetpoint;
+inputs.appliedCurrent;
+inputs.appliedVoltage;
     }
 }
