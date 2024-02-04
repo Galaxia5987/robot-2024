cded1fb0b5a736950def777d1e1aef1609824df8
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-04 21:42:25 +0200

    Fix some very weird conflicts

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,9 +3,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.conveyor.Conveyor;
-import frc.robot.subsystems.conveyor.ConveyorConstants;
-import frc.robot.subsystems.conveyor.ConveyorIO;
-import frc.robot.subsystems.conveyor.ConveyorIOSim;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.ElevatorIO;
 import frc.robot.subsystems.elevator.ElevatorIOReal;
@@ -22,39 +19,27 @@ public class RobotContainer {
     private final SwerveDrive swerveDrive;
     private final CommandXboxController xboxController = new CommandXboxController(0);
 
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
     private RobotContainer() {
         GripperConstants.initConstants();
         GripperIO gripperIO;
         ElevatorIO elevatorIO;
-        ExampleSubsystemIO exampleSubsystemIO;
         switch (Constants.CURRENT_MODE) {
             case REAL:
                 elevatorIO = new ElevatorIOReal();
-                exampleSubsystemIO = new ExampleSubsystemIOReal();
-                ConveyorIO conveyorIO;
-                ConveyorConstants.initialize(Constants.CURRENT_MODE);
-                switch (Constants.CURRENT_MODE) {
-                    case REAL:
-                        elevatorIO = new ElevatorIOReal();
-                        conveyorIO = new ConveyorIOSim();
-                        break;
-                    case SIM:
-                    case REPLAY:
-                    default:
-                        exampleSubsystemIO = new ExampleSubsystemIOSim();
-                        elevatorIO = new ElevatorIOSim();
-                        break;
-                }
-                ExampleSubsystem.initialize(exampleSubsystemIO);
-                Elevator.initialize(elevatorIO);
-                conveyorIO = new ConveyorIOSim();
+                gripperIO = new GripperIOReal();
+                break;
+            case SIM:
+            case REPLAY:
+            default:
+                elevatorIO = new ElevatorIOSim();
                 elevatorIO = new ElevatorIOSim();
                 gripperIO = new GripperIOSim();
                 break;
         }
         Elevator.initialize(elevatorIO);
-        Conveyor.initialize(conveyorIO);
         Gripper.initialize(gripperIO);
         Constants.initSwerve();
         Constants.initVision();
@@ -86,7 +71,8 @@ public class RobotContainer {
                         () -> true));
     }
 
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+    }
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
