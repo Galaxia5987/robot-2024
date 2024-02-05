10c14aa9e078561a9d3baf56f41531141f4f619e
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-05 20:50:42 +0200

    Add optimal angle to speaker calculation in ShootState

--- src/main/java/frc/robot/scoreStates/ShootState.java
+++ src/main/java/frc/robot/scoreStates/ShootState.java
@@ -2,11 +2,16 @@ package frc.robot.scoreStates;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
 import frc.robot.swerve.SwerveDrive;
+import java.util.List;
 import java.util.Set;
+import lib.Utils;
 
 public class ShootState implements ScoreState {
 
@@ -14,15 +19,31 @@ public class ShootState implements ScoreState {
     public Command driveToClosestOptimalPoint() {
         return Commands.defer(
                 () -> {
-                    var optimalPoints =
-                            isRed()
-                                    ? ScoreStateConstants.OPTIMAL_POINTS_SHOOT_RED
-                                    : ScoreStateConstants.OPTIMAL_POINTS_SHOOT_BLUE;
-                    Pose2d optimalPose =
-                            SwerveDrive.getInstance().getBotPose().nearest(optimalPoints);
-                    return AutoBuilder.pathfindToPose(optimalPose, Constants.AUTO_CONSTRAINTS);
+                    Translation2d speakerPose;
+                    Pose2d botPose = SwerveDrive.getInstance().getBotPose();
+                    List<Translation2d> optimalPoints;
+                    if (isRed()) {
+                        speakerPose = ScoreStateConstants.SPEAKER_POSE_RED;
+                        optimalPoints = ScoreStateConstants.OPTIMAL_POINTS_SHOOT_RED;
+                    } else {
+                        speakerPose = ScoreStateConstants.SPEAKER_POSE_BLUE;
+                        optimalPoints = ScoreStateConstants.OPTIMAL_POINTS_SHOOT_BLUE;
+                    }
+                    var optimalTranslation =
+                            Utils.getDistanceFromPoint(speakerPose, botPose)
+                                            < ScoreStateConstants.MAX_SHOOTING_DISTANCE.in(
+                                                    Units.Meters)
+                                    ? botPose.getTranslation()
+                                    : botPose.getTranslation().nearest(optimalPoints);
+                    Rotation2d optimalRotation =
+                            new Rotation2d(
+                                    speakerPose.getX() - optimalTranslation.getX(),
+                                    speakerPose.getY() - optimalTranslation.getY());
+                    return AutoBuilder.pathfindToPose(
+                            new Pose2d(optimalTranslation, optimalRotation),
+                            Constants.AUTO_CONSTRAINTS);
                 },
-                DTOP_REQUIREMENTS);
+                Set.of(SwerveDrive.getInstance()));
     }
 
     @Override
