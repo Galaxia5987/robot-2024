7c04aa1a6e0c4eab0568d226017aedd7e4efd162
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-06 21:37:20 +0200

    A lot of fun

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -2,11 +2,19 @@ package frc.robot.subsystems.gripper;
 
 import static frc.robot.subsystems.intake.IntakeConstants.*;
 
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.units.*;
 import frc.robot.Constants;
+import frc.robot.subsystems.hood.HoodConstants;
 import lib.webconstants.LoggedTunableNumber;
 
 public class GripperConstants {
+    public static final TalonFXConfiguration MOTOR_CONFIGURATION = new TalonFXConfiguration();
+
     public static final Measure<Angle> INTAKE_ANGLE = null;
     public static final Measure<Angle> OUTTAKE_ANGLE = null;
     public static final double INTAKE_POWER = 0;
@@ -16,25 +24,55 @@ public class GripperConstants {
     public static final Measure<Distance> GRIPPER_POSITION_Y = Units.Meters.of(0);
     public static final Measure<Distance> GRIPPER_POSITION_z = Units.Meters.of(0.6461);
     public static final double ANGLE_MOTOR_GEAR_RATIO = 58.5;
+    public static final InvertedValue ANGLE_INVERTED_VALUE = InvertedValue.Clockwise_Positive;
+    public static final boolean ROLLER_INVERTED_VALUE = true;
+    public static final int CURRENT_LIMIT = 40;
+
 
     public static final LoggedTunableNumber KP = new LoggedTunableNumber("Gripper/kP");
     public static final LoggedTunableNumber KI = new LoggedTunableNumber("Gripper/kI");
     public static final LoggedTunableNumber KD = new LoggedTunableNumber("Gripper/kD");
+    public static final LoggedTunableNumber KV = new LoggedTunableNumber("Gripper/kD");
+    public static final LoggedTunableNumber KA = new LoggedTunableNumber("Gripper/kD");
+    public static final LoggedTunableNumber KG = new LoggedTunableNumber("Gripper/kD");
 
     public static void initConstants() {
         switch (Constants.CURRENT_MODE) {
             case REAL:
-                KP.initDefault(0);
+                KP.initDefault(0.2);
                 KI.initDefault(0);
                 KD.initDefault(0);
+                KV.initDefault(0);
+                KA.initDefault(0);
+                KG.initDefault(0);
                 break;
             case SIM:
             case REPLAY:
             default:
                 KP.initDefault(24);
-                KI.initDefault(0.000_001);
+                KI.initDefault(0.000001);
                 KD.initDefault(0);
                 break;
         }
+
+        MOTOR_CONFIGURATION
+                .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(GEAR_RATIO))
+                .withSlot0(
+                        new Slot0Configs()
+                                .withKP(GripperConstants.KP.get())
+                                .withKI(GripperConstants.KI.get())
+                                .withKD(GripperConstants.KD.get())
+                                .withKV(GripperConstants.KV.get())
+                                .withKA(GripperConstants.KA.get())
+                                .withKG(GripperConstants.KG.get()))
+                .withMotorOutput(new MotorOutputConfigs().withInverted(ANGLE_INVERTED_VALUE))
+                .CurrentLimits
+                .withStatorCurrentLimitEnable(true)
+                .withSupplyCurrentLimitEnable(true)
+                .withStatorCurrentLimit(CURRENT_LIMIT)
+                .withSupplyCurrentLimit(CURRENT_LIMIT);
     }
+
+
+
 }
--- src/main/java/frc/robot/subsystems/gripper/GripperIOReal.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperIOReal.java
@@ -1,18 +1,68 @@
 package frc.robot.subsystems.gripper;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import frc.robot.subsystems.elevator.ElevatorIO;
 
 public class GripperIOReal implements GripperIO {
-    @Override
-    public void setRollerMotorPower(double power) {}
+    private final TalonFX angleMotor;
+    private final CANSparkMax rollerMotor;
+    private final DigitalInput sensor;
+    private final DutyCycleEncoder absoluteEncoder = new DutyCycleEncoder(0);
+    private final MotionMagicExpoTorqueCurrentFOC positionRequest = new MotionMagicExpoTorqueCurrentFOC(0);
+    private final DutyCycleOut powerRequest = new DutyCycleOut(0);
+
+    private GripperIOReal(){
+        angleMotor = new TalonFX(0);
+        angleMotor.getConfigurator().apply(GripperConstants.MOTOR_CONFIGURATION);
+        rollerMotor = new CANSparkMax(0, CANSparkLowLevel.MotorType.kBrushless);
+        sensor = new DigitalInput(0);
+        rollerMotor.setSmartCurrentLimit(GripperConstants.CURRENT_LIMIT);
+        rollerMotor.setInverted(GripperConstants.ROLLER_INVERTED_VALUE);
+        rollerMotor.burnFlash();
+    }
 
     @Override
-    public void setAngleMotorPower(double power) {}
+    public void setRollerMotorPower(double power) {
+        inputs.rollerPowerSetpoint = power;
+        rollerMotor.set(power);
+    }
 
     @Override
-    public void setAngle(MutableMeasure<Angle> angle) {}
+    public void setAngleMotorPower(double power) {
+        angleMotor.setControl(powerRequest.withOutput(power));
+    }
 
     @Override
-    public void updateInputs() {}
+    public void setAngle(MutableMeasure<Angle> angle) {
+    }
+
+    public boolean hasNote(){
+       return sensor.get();
+    }
+
+    public void stopWrist(){
+        angleMotor.stopMotor();
+    }
+
+    public void stopRollers(){
+        rollerMotor.stopMotor();
+    }
+
+    @Override
+    public void updateInputs() {
+        inputs.angleMotorVoltage.mut_replace(angleMotor.get(), Units.Volts);
+        inputs.rollerMotorVoltage.mut_replace(rollerMotor.get(), Units.Volts);
+        inputs.currentAngle.mut_replace(angleMotor.getPosition().getValue(), Units.Degrees);
+        inputs.hasNote = hasNote();
+    }
 }
