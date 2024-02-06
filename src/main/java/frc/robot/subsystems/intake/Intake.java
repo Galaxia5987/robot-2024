667493d76d407010395b81ad8bfec12a29abe213
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-06 21:21:45 +0200

    Change intake & stop commands to use Commands.parallel

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -56,14 +56,16 @@ public class Intake extends SubsystemBase {
     }
 
     public Command intake() {
-        return setAngle(IntakePose.DOWN)
-                .alongWith(setRollerSpeed(0.5).alongWith(setCenterRollerSpeed(0.5)))
+        return Commands.parallel(
+                        setAngle(IntakePose.DOWN),
+                        setRollerSpeed(0.5).alongWith(setCenterRollerSpeed(0.5)))
                 .withName("feeding position activated");
     }
 
     public Command stop() {
-        return setAngle(IntakePose.UP)
-                .alongWith(setRollerSpeed(0).alongWith(setCenterRollerSpeed(0)))
+        return Commands.parallel(
+                        setAngle(IntakePose.UP),
+                        setRollerSpeed(0).alongWith(setCenterRollerSpeed(0)))
                 .withName("stopped");
     }
 
