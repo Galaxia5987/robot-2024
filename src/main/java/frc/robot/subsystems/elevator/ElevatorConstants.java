98d40bfcedb964508448f5db0f967e530ebb7771
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-07 20:28:01 +0200

    Create elevatorOuttakeHeight constant

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -11,11 +11,14 @@ public class ElevatorConstants { // TODO: check real values
     public static final double GEAR_RATIO = 12.0;
     public static final double DRUM_RADIUS = 0.02; // [m]
 
+    // TODO: replace with actual values
     public static final MutableMeasure<Distance> STARTING_HEIGHT =
             Units.Meters.of(0).mutableCopy(); // [m]
     public static final MutableMeasure<Distance> MIN_HEIGHT =
             Units.Meters.of(0).mutableCopy(); // [m]
     public static final MutableMeasure<Distance> MAX_HEIGHT =
+            Units.Meters.of(0).mutableCopy(); // [m]
+    public static final MutableMeasure<Distance> OUTTAKE_HEIGHT =
             Units.Meters.of(0).mutableCopy(); // [m]
 
     public static final double MAX_VELOCITY = 3;
