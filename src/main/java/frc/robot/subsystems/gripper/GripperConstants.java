1963f081fd2bfd71ff037a3288aeac5fd4fcd3e8
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-07 10:48:16 +0200

    Add amp power constants

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -7,10 +7,12 @@ import frc.robot.Constants;
 import lib.webconstants.LoggedTunableNumber;
 
 public class GripperConstants {
-    public static final Measure<Angle> INTAKE_ANGLE = null;
-    public static final Measure<Angle> OUTTAKE_ANGLE = null;
-    public static final double INTAKE_POWER = 0;
-    public static final double OUTTAKE_POWER = 0;
+    public static final Measure<Angle> INTAKE_ANGLE = null; // TODO: replace with actual value
+    public static final Measure<Angle> OUTTAKE_ANGLE = null; // TODO: replace with actual value
+    public static final double INTAKE_POWER = 0; // TODO; replace with actual value
+    public static final double OUTTAKE_POWER = 0; // TODO: replace with actual value
+    public static final double AMP_POWER_NORMAL = 0;
+    public static final double AMP_POWER_REVERSE = 0;
     public static final Measure<Dimensionless> THRESHOLD = Units.Percent.of(0.02);
     public static final Measure<Distance> GRIPPER_POSITION_X = Units.Meters.of(0);
     public static final Measure<Distance> GRIPPER_POSITION_Y = Units.Meters.of(0);
