6ed67d66c65e9189efcf3157c39ccedc7040b903
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-07 10:46:59 +0200

    Add isAmpingForward

--- src/main/java/frc/robot/scoreStates/AmpState.java
+++ src/main/java/frc/robot/scoreStates/AmpState.java
@@ -8,34 +8,46 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.ElevatorConstants;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.gripper.GripperConstants;
 import frc.robot.swerve.SwerveDrive;
 import java.util.Set;
 import lib.Utils;
 
 public class AmpState implements ScoreState {
+    private static Elevator elevator;
+    private static Gripper gripper;
+    private static boolean isAmpingForward = true;
+
+    private AmpState() {
+        elevator = Elevator.getInstance();
+        gripper = Gripper.getInstance();
+    }
 
     @Override
     public Command driveToClosestOptimalPoint() {
         return Commands.defer(
                 () -> {
                     Translation2d ampPose;
+                    isAmpingForward = true;
                     if (isRed()) {
                         ampPose = ScoreStateConstants.AMP_POSE_RED;
                     } else {
                         ampPose = ScoreStateConstants.AMP_POSE_BLUE;
                     }
-                    Rotation2d ampRotation = ScoreStateConstants.AMP_ROTATION_NORMAL;
+                    Rotation2d ampRotation;
+                    ampRotation = ScoreStateConstants.AMP_ROTATION_NORMAL;
                     Pose2d botPose = SwerveDrive.getInstance().getBotPose();
                     double robotRotation = botPose.getRotation().getRadians();
-                    boolean isGripperReversed =
-                            false; // TODO: replace with actual gripper.isForward
                     double distance = Utils.getDistanceFromPoint(ampPose, botPose);
                     boolean hasTimeToTurnGripper =
                             distance
                                     > ScoreStateConstants.MIN_DISTANCE_TO_TURN_GRIPPER.in(
                                             Units.Meters);
-
-                    if ((isGripperReversed && robotRotation < 0) || hasTimeToTurnGripper) {
+                    if ((gripper.isReversed() && robotRotation < 0) || hasTimeToTurnGripper) {
+                        isAmpingForward = false;
                         ampRotation = ScoreStateConstants.AMP_ROTATION_REVERSE;
                     }
                     return AutoBuilder.pathfindToPose(
