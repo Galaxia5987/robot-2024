c8de30fc9e890f971902549f624fb53d36c06f9b
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-07 10:48:32 +0200

    Add wrist positions

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -19,6 +19,14 @@ public class GripperConstants {
     public static final Measure<Distance> GRIPPER_POSITION_z = Units.Meters.of(0.6461);
     public static final double ANGLE_MOTOR_GEAR_RATIO = 58.5;
 
+    public static final Measure<Angle> WRIST_FOLD_POSITION =
+            Units.Rotations.of(0.5); // TODO: replace with actual value
+    public static final Measure<Angle> WRIST_FORWARD_AMP_POSE =
+            Units.Rotations.of(0.3); // TODO: replace with actual value;
+    public static final Measure<Angle> WRIST_BACKWARDS_AMP =
+            Units.Rotations.of(0.6); // TODO: replace with actual value;
+    public static final Measure<Angle> WRIST_TOLERANCE = Units.Rotations.of(0.05);
+
     public static final LoggedTunableNumber KP = new LoggedTunableNumber("Gripper/kP");
     public static final LoggedTunableNumber KI = new LoggedTunableNumber("Gripper/kI");
     public static final LoggedTunableNumber KD = new LoggedTunableNumber("Gripper/kD");
