6a6d14645ec3b2560832188fd593e35f5a7736b3
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-07 19:42:11 +0200

    Write actual values

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -67,11 +67,11 @@ public class GripperConstants {
                                 .withKA(GripperConstants.KA.get())
                                 .withKG(GripperConstants.KG.get())
                                 .withGravityType(GravityTypeValue.Arm_Cosine))
-                .withMotorOutput(new MotorOutputConfigs().withInverted(ANGLE_INVERTED_VALUE))
+                .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive))
                 .CurrentLimits
                 .withStatorCurrentLimitEnable(true)
                 .withSupplyCurrentLimitEnable(true)
-                .withStatorCurrentLimit(CURRENT_LIMIT)
-                .withSupplyCurrentLimit(CURRENT_LIMIT);
+                .withStatorCurrentLimit(40)
+                .withSupplyCurrentLimit(50);
     }
 }
