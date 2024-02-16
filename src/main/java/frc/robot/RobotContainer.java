48f6824d4eca5b07642d9ffefb3da44e551ea7f5
Alon Schwierz <@1>
2024-02-16 13:40:52 +0200

    still not working

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,10 +3,12 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commandGroups.CommandGroups;
 import frc.robot.lib.GalacticProxyCommand;
@@ -115,6 +117,7 @@ public class RobotContainer {
 
         NamedCommands.registerCommand("intake", commandGroups.intake());
         NamedCommands.registerCommand("shoot", commandGroups.autoShoot());
+//        NamedCommands.registerCommand("resetPose", new InstantCommand(()->swerveDrive.resetPose(PathPlannerAuto.getStaringPoseFromAutoFile("LowerFullWing"))));
 
         autoChooser = AutoBuilder.buildAutoChooser();
         autoChooser.addOption("LowerFullWing", new PathPlannerAuto("LowerFullWing"));
