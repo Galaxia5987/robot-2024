19d2c5b5a2f36cc396ee451ef52b8bdd59fe3e0f
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-24 14:42:20 +0200

    Remove unnecessary line

--- src/main/java/frc/robot/subsystems/leds/LEDs.java
+++ src/main/java/frc/robot/subsystems/leds/LEDs.java
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import java.util.function.IntSupplier;
 
 public class LEDs extends SubsystemBase {
