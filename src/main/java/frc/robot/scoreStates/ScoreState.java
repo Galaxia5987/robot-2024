f9ac0ad41538919523798af5693ec15bf7a9d9f8
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-26 20:14:35 +0200

    Add getSwerveAngle to ScoreState

--- src/main/java/frc/robot/scoreStates/ScoreState.java
+++ src/main/java/frc/robot/scoreStates/ScoreState.java
@@ -1,5 +1,8 @@
 package frc.robot.scoreStates;
 
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -16,5 +19,13 @@ public interface ScoreState {
         return Commands.none();
     }
 
-    Command score(Optional<CommandXboxController> driveController);
+    default Command prepareSubsytems() {
+        return Commands.none();
+    }
+
+    Command score(Optional<CommandXboxController> driveController, boolean isAuto);
+
+    default MutableMeasure<Angle> getSwerveAngle() {
+        return Units.Degrees.of(0).mutableCopy();
+    }
 }
