f0ebbc595b27123bd14eeb17958a4246487976d7
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-26 10:49:02 +0200

    Change ScoreState interface

--- src/main/java/frc/robot/scoreStates/ScoreState.java
+++ src/main/java/frc/robot/scoreStates/ScoreState.java
@@ -3,6 +3,8 @@ package frc.robot.scoreStates;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import java.util.Optional;
 
 public interface ScoreState {
     static boolean isRed() {
@@ -10,9 +12,9 @@ public interface ScoreState {
         return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red;
     }
 
-    default Command updateState() {
+    default Command calculateTargets() {
         return Commands.none();
     }
 
-    Command score();
+    Command score(Optional<CommandXboxController> driveController);
 }
