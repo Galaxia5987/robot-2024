e8b1b24fb85aba60dff3382b81bf244764ef88f6
GaiaZano05 <gaya.zano11@gmail.com>
2024-02-29 11:59:28 +0200

    Add outOfBounds function to VisionConstants

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -1,3 +1,16 @@
 package frc.robot.subsystems.vision;
 
-public class VisionConstants {}
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+
+public class VisionConstants {
+
+    public static final double FIELD_LENGTH = 16.54;
+    public static final double FIELD_WIDTH = 8.23;
+
+    public static boolean outOfBounds(Pose3d estimatedPose) {
+        return estimatedPose.getX() < 0 || estimatedPose.getX() > FIELD_LENGTH
+                || estimatedPose.getY() < 0 || estimatedPose.getY() > FIELD_WIDTH;
+    }
+}
