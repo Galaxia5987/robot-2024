2feac67969a8a865d879ea8319d6cc6d82bbdc87
GaiaZano05 <gaya.zano11@gmail.com>
2024-03-02 16:46:47 +0200

    Yahoo answers

--- src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -140,7 +140,7 @@ public class SwerveConstants {
 
                 ROBOT_WIDTH = 0.585;
                 ROBOT_LENGTH = 0.585;
-                WHEEL_DIAMETER = 0.099;
+                WHEEL_DIAMETER = 0.102;
                 DRIVE_REDUCTION = (1 / 2.0) * (24.0 / 22.0) * (15.0 / 45.0);
                 ANGLE_REDUCTION = (14.0 / 72.0) * 0.5;
 
