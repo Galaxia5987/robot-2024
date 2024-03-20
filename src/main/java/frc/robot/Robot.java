36ac911bd3c673c06c1f47118c53554a5a59f084
GaiaZano05 <gaya.zano11@gmail.com>
2024-03-20 07:44:07 +0200

    Remove usb logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commandGroups.CommandGroups;
 import frc.robot.commandGroups.CommandGroupsConstants;
 import frc.robot.lib.PoseEstimation;
 import frc.robot.subsystems.ShootingManager;
@@ -81,7 +80,6 @@ public class Robot extends LoggedRobot {
                 LoggedPowerDistribution.getInstance();
                 Logger.addDataReceiver(new NT4Publisher());
                 Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs"));
-                //                Logger.addDataReceiver(new WPILOGWriter());
                 break;
             case SIM:
                 Logger.addDataReceiver(new NT4Publisher());
