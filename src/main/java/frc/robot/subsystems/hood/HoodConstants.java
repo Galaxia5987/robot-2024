7ac1540f8da0790643007546fd7a6fea1da80e73
GaiaZano05 <gaya.zano11@gmail.com>
2024-03-20 15:03:01 +0200

    Spotless

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -63,7 +63,7 @@ public class HoodConstants {
                 kV.initDefault(0);
                 kA.initDefault(0.0);
                 kG.initDefault(9.0);
-                ABSOLUTE_ENCODER_OFFSET.initDefault((155.91796875 - 33.48) / 360.0);
+                ABSOLUTE_ENCODER_OFFSET.initDefault((155.917_968_75 - 33.48) / 360.0);
             case SIM:
             case REPLAY:
                 kP.initDefault(10);
