37087f587f66fb6994eefb4374b689d8ea707e9b
GaiaZano05 <gaya.zano11@gmail.com>
2024-03-21 10:04:53 +0200

    Update hood offset

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -63,7 +63,7 @@ public class HoodConstants {
                 kV.initDefault(0);
                 kA.initDefault(0.0);
                 kG.initDefault(9.0);
-                ABSOLUTE_ENCODER_OFFSET.initDefault((120.76 - 33.48) / 360.0);
+                ABSOLUTE_ENCODER_OFFSET.initDefault((76.81 - 33.48) / 360.0);
             case SIM:
             case REPLAY:
                 kP.initDefault(10);
