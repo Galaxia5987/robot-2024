6020e8dd45cd500bae36a691e7e56b17dc72d82d
eitan <eitan.vichik@gmail.com>
2024-03-29 14:06:43 +0300

    Change absolute encoder offset

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -63,7 +63,7 @@ public class HoodConstants {
                 kV.initDefault(0);
                 kA.initDefault(0.0);
                 kG.initDefault(9.0);
-                ABSOLUTE_ENCODER_OFFSET.initDefault((77.08 - 33.48) / 360.0);
+                ABSOLUTE_ENCODER_OFFSET.initDefault((76.9 - 33.48) / 360.0);
             case SIM:
             case REPLAY:
                 kP.initDefault(10);
