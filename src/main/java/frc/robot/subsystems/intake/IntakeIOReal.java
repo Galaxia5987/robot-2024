f83eb70830a2a80c6152dae6243a3e262b5daf34
yahali david mor <mor.yahali1612@gmail.com>
2024-05-05 17:44:19 +0300

    update

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -1,82 +1,30 @@
 package frc.robot.subsystems.intake;
 
-import static frc.robot.subsystems.intake.IntakeConstants.*;
-
-import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.*;
-import edu.wpi.first.units.*;
-import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.Constants;
-import frc.robot.Ports;
 
 public class IntakeIOReal implements IntakeIO {
+    private final TalonFX angleMotor = new TalonFX(IntakeConstants.angleMotorDeviceID);
+    private final TalonFX spinMotor = new TalonFX(IntakeConstants.spinMotorDeviceID);
+    private final VoltageOut voltageOutput = new VoltageOut(.0);
+    private final PositionVoltage positionVoltageOutput = new PositionVoltage(.0);
 
-    public final CANSparkMax spinMotor =
-            new CANSparkMax(Ports.Intake.ROLLER_ID, CANSparkLowLevel.MotorType.kBrushless);
-    private final CANSparkMax centerMotor =
-            new CANSparkMax(Ports.Intake.CENTER_ID, CANSparkLowLevel.MotorType.kBrushless);
-    private final TalonFX angleMotor = new TalonFX(Ports.Intake.ANGLE_ID);
-    private final PositionVoltage positionRequest = new PositionVoltage(0).withEnableFOC(true);
-
-    public IntakeIOReal() {
-        angleMotor.getConfigurator().apply(ANGLE_CONFIGURATION);
-
-        centerMotor.restoreFactoryDefaults();
-        centerMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
-        centerMotor.enableVoltageCompensation(Constants.NOMINAL_VOLTAGE.in(Units.Volts));
-        centerMotor.setInverted(false);
-        centerMotor.setSmartCurrentLimit(CENTER_CURRENT_LIMIT);
-        centerMotor.setIdleMode(CANSparkBase.IdleMode.kCoast);
-        spinMotor.restoreFactoryDefaults();
-        spinMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
-        spinMotor.enableVoltageCompensation(Constants.NOMINAL_VOLTAGE.in(Units.Volts));
-        spinMotor.setInverted(false);
-        spinMotor.setSmartCurrentLimit(SPIN_CURRENT_LIMIT);
-        spinMotor.burnFlash();
-        centerMotor.burnFlash();
-
-        angleMotor.setPosition(IntakePose.UP.intakePose.in(Units.Degrees));
-
-        BaseStatusSignal.setUpdateFrequencyForAll(
-                100, angleMotor.getPosition(), angleMotor.getMotorVoltage());
+    @Override
+    public void updateInput() {
+        inputs.angle = angleMotor.getPosition().getValue() * Constants.toRadiansFromRotation;
+        inputs.spinMotorVoltage = spinMotor.getSupplyVoltage().getValue();
+        inputs.angleMotorVoltage = angleMotor.getSupplyVoltage().getValue();
     }
 
     @Override
-    public void setAngle(MutableMeasure<Angle> angle) {
-        angleMotor.setControl(positionRequest.withPosition(angle.in(Units.Degrees)));
+    public void setAngle(double angle) {
+        angleMotor.setControl(positionVoltageOutput.withPosition(angle));
     }
 
     @Override
-    public void setRollerSpeed(double speed) {
-        spinMotor.set(speed);
-    }
-
-    @Override
-    public void setCenterRollerSpeed(double speed) {
-        centerMotor.set(speed);
-    }
-
-    @Override
-    public void reset(Measure<Angle> angle) {
-        angleMotor.setPosition(angle.in(Units.Degrees));
-    }
-
-    @Override
-    public void setAnglePower(double power) {
-        angleMotor.set(power);
-    }
-
-    @Override
-    public void updateInputs() {
-        inputs.currentAngle.mut_replace((angleMotor.getPosition().getValue()), Units.Degrees);
-        inputs.angleMotorVoltage.mut_replace(
-                (angleMotor.getMotorVoltage().getValue()), Units.Volts);
-        inputs.spinMotorVoltage.mut_replace(
-                (spinMotor.getAppliedOutput() * RobotController.getBatteryVoltage()), Units.Volts);
-        inputs.centerMotorVoltage.mut_replace(
-                (centerMotor.getAppliedOutput() * RobotController.getBatteryVoltage()),
-                Units.Volts);
+    public void setVoltage(double voltage) {
+        spinMotor.setControl(voltageOutput.withOutput(voltage));
     }
 }
--- src/main/java/frc/robot/subsystems/leds/LedConstants.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.leds;
-
-public class LedConstants {
-    public static final double MINIMAL_BLINK_TIME = 0.1; // [sec]
-}
